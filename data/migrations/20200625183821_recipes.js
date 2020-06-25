1398fa3be2aabba8f24bd1217a98c0dcd8f8dcb7
Cobrettie <cobrettiegarner3@gmail.com>
2020-06-25 19:32:33 -0400

    configure boilerplate tables for recipe, ingredient, steps

--- data/migrations/20200625183821_recipes.js
+++ data/migrations/20200625183821_recipes.js
@@ -10,16 +10,40 @@ exports.up = function(knex) {
         .unsigned()
         .notNullable()
         .references('id')
-        .inTable('ingredients')
+        .inTable('ingredient')
         .onUpdate('CASCADE')
         .onDelete('CASCADE');
       table.integer('quantity')
         .notNullable();
   })
+
+  .createTable('ingredient', table => {
+    table.increments();
+    table.string('ingredient_name')
+      .notNullable();
+  })
+
+  .createTable('steps', table => {
+    table.increments();
+    table.integer('recipe_id')
+      .unsigned()
+      .notNullable()
+      .references('id')
+      .inTable('recipe')
+      .onUpdate('CASCADE')
+      .onDelete('CASCADE');
+    table.string('step_name')
+      .notNullable();
+    table.string('step_description')
+      .notNullable();
+  })
 };
 
 exports.down = function(knex) {
-  
+  return knex.schema
+    .dropTableIfExists('recipe')
+    .dropTableIfExists('ingredient')
+    .dropTableIfExists('steps')
 };
 
 
